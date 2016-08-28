79b72c4b547b33edfe4ccfdbd7fc768bb8252d70
Eden Toledano <edentoledano@gmail.com>
2016-08-28 23:11:16 +0300

    refacoring

--- src/filesystem/UI.java
+++ src/filesystem/UI.java
@@ -16,49 +16,48 @@ public class UI {
         this.fileSystem = fileSystem;
     }
 
-    private void addFile(){
+    private void addFile() {
         String fileName = parseFileName();
         if (fileName == null) return;
 
-        String directory = parseDirName();
-        System.out.println("Enter the size of " + fileName +" : ");
+        String directory = parseParentDirName();
+        System.out.println("Enter the size of " + fileName + " : ");
         String size = scanner.nextLine();
 
-        if (isPathValid(fileName, directory)){
+        if (isPathValid(fileName, directory)) {
             try {
                 int sizeInt = Integer.parseInt(size);
                 fileSystem.addFile(directory, fileName, sizeInt);
                 System.out.println("File Added.");
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Size must be numeric. Failed to add new file. try again... ");
             }
-        }
-        else{
+        } else {
             System.out.println(String.format("Error: File name '%s' is already in use or '%s' is not a directory", fileName, directory));
         }
 
     }
 
-    private String parseDirName() {
-        System.out.println("Enter parent directory name (or 'root' if you have not added any): ");
-        return scanner.nextLine();
-    }
-
-    private void addDirectory(){
+    private void addDirectory() {
         String newDir = parseFileName();
         if (newDir == null) return;
-        String parentDir = parseDirName();
+        String parentDir = parseParentDirName();
 
-        if(isPathValid(newDir, parentDir)){
+        if (isPathValid(newDir, parentDir)) {
             fileSystem.addDirectory(parentDir, newDir);
             System.out.println("Directory Added.");
-        }
-        else{
+        } else {
             System.out.println(String.format("Error: Directory name '%s' is already in use or '%s' is not a directory", newDir, parentDir));
         }
 
     }
-    private boolean isPathValid(String newFile, String parent){
+
+    private String parseParentDirName() {
+        System.out.println("Enter parent directory name (or 'root' if you have not added any): ");
+        return scanner.nextLine();
+    }
+
+    private boolean isPathValid(String newFile, String parent) {
 
         return (!fileSystem.isNameTaken(newFile) && fileSystem.isDirectory(parent));
     }
@@ -66,35 +65,42 @@ public class UI {
     private String parseFileName() {
         System.out.println("Enter new file/directory name: ");
         String fileName = scanner.nextLine();
-        if (fileName.length() > 32){
-            System.out.println("Error: file name cannot be more than 32.");
+        if (!isValidLength(fileName)) {
             return null;
         }
         return fileName;
     }
 
-    private void delete(){
+    private boolean isValidLength(String name) {
+        int MAX_NAME_LENGTH = 32;
+        if (name != null && (name.length() > MAX_NAME_LENGTH)) {
+            System.out.println("Error: file name cannot be more than " + MAX_NAME_LENGTH);
+            return false;
+        }
+        return true;
+    }
+
+    private void delete() {
         System.out.println("Enter file name to remove its parent directory: ");
         String fileName = scanner.nextLine();
 
-        if (fileSystem.isNameTaken(fileName) && !fileSystem.isDirectory(fileName)){
-            if(fileSystem.delete(fileName)){
+        if (fileSystem.isNameTaken(fileName) && !fileSystem.isDirectory(fileName)) {
+            if (fileSystem.delete(fileName)) {
                 System.out.println(String.format("Parent directory of %s was removed successfully", fileName));
             }
-        }
-        else{
+        } else {
             System.out.println(String.format("Error:'%s' is not a file or it could not be found.", fileName));
 
         }
 
     }
 
-    private void showFileSystem(){
+    private void showFileSystem() {
         fileSystem.showFileSystem();
         System.out.println("\n");
     }
 
-    private void showActions(){
+    private void showActions() {
 
         List<String> actions = new ArrayList<>();
 
@@ -115,8 +121,7 @@ public class UI {
 
     }
 
-    public void start(){
-
+    public void start() {
 
 
         System.out.print("-------------------------- WELCOME ----------------------------\n");
